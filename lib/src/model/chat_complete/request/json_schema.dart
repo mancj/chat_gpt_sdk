8818df1e350f185b8c6942b8108e92bc1d11553c
mancj <mansur.ekaev@lab80.io>
2025-06-12 02:31:52 +0300

    feat: make strict parameter optional in JsonSchema constructor

--- lib/src/model/chat_complete/request/json_schema.dart
+++ lib/src/model/chat_complete/request/json_schema.dart
@@ -1,9 +1,9 @@
 class JsonSchema {
   final String name;
   final Map<String, dynamic> schema;
-  final bool strict = true;
+  final bool strict;
 
-  JsonSchema({required this.name, required this.schema});
+  JsonSchema({required this.name, required this.schema, this.strict = true});
 
   Map<String, dynamic> toJson() {
     final data = <String, dynamic>{};
